f6c06baf5c9e3a30e35fc2e7a76af8684c0d5dbd
Berg45 <pereiraberg62@gmail.com>
2024-03-26 16:59:49 -0300

    alterações projeto

--- Módulo 02/07 - Interação com API/Aula 06/exemplo 04 copy 3/detalhes.js
+++ Módulo 02/07 - Interação com API/Aula 06/exemplo 04 copy 3/detalhes.js
@@ -1,4 +1,5 @@
 async function buscarDetalhes(){
+
     let busca = await fetch("lista-produtos.json")
     let produtos = await busca.json()
 
@@ -6,26 +7,24 @@ async function buscarDetalhes(){
     let parametroID = paramentros.get("produto-id")
 
     let indiceProd
-
     for(let x in produtos){
         if(produtos[x].id == parametroID){
             indiceProd = x
         }
-    
     }
+
     document.body.innerHTML = `
     <div class="detalhes">
         <h3>${produtos[indiceProd].nome}</h3>
-        <img src="${produtos[indiceProd].img}" width="250" height="250">
+        <img src="${produtos[indiceProd].img}" widtm="250" height="250">
         <p>${produtos[indiceProd].descricao}</p>
-
-        <div class="valor">
-            <span class="valorCo"> R$ ${(produtos[indiceProd].valorComDesconto.toFixed(2)).replace(".", ",")}</span>
-            <span class="valorSe"> R$ ${(produtos[indiceProd].valorSemDesconto.toFixed(2)).replace(".", ",")}</span>
+        <div class="valores">
+             <span class="valorCo">R$ ${produtos[indiceProd].valorComDesconto.toFixed(2).replace("." , ",")}</span>
+             <span class="valorSe">R$ ${produtos[indiceProd].valorSemDesconto.toFixed(2).replace("." , ",")}</span>
         </div>
     </div>
+    
     `
-   
-
 }
+
 buscarDetalhes()
